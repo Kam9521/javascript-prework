b0bf2b1ba2ec46594835cd0449824a1d33360688
Kamil <szpmytyl@wp.pl>
2026-03-24 18:41:11 +0100

    Refactor code using functions getMoveName and displayResult

--- js/script.js
+++ js/script.js
@@ -1,50 +1,51 @@
-// Clear messages at the start
+// Function returns move name based on number
+function getMoveName(argMoveId) {
+  if (argMoveId == 1) {
+    return "rock";
+  } else if (argMoveId == 2) {
+    return "paper";
+  } else if (argMoveId == 3) {
+    return "scissors";
+  } else {
+    printMessage("Unknown move id: " + argMoveId);
+    return "unknown move";
+  }
+}
+
+// Function displays game result
+function displayResult(argComputerMove, argPlayerMove) {
+  printMessage("Computer played: " + argComputerMove);
+  printMessage("You played: " + argPlayerMove);
+
+  if (argComputerMove == argPlayerMove) {
+    printMessage("Draw!");
+  } else if (argComputerMove == "rock" && argPlayerMove == "paper") {
+    printMessage("You win!");
+  } else if (argComputerMove == "paper" && argPlayerMove == "scissors") {
+    printMessage("You win!");
+  } else if (argComputerMove == "scissors" && argPlayerMove == "rock") {
+    printMessage("You win!");
+  } else if (argPlayerMove == "unknown move") {
+    printMessage("Wrong input!");
+  } else {
+    printMessage("Computer wins!");
+  }
+}
+
+// Clear messages
 clearMessages();
 
-// Random number from 1 to 3
+// Random number for computer
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-// Computer move
-let computerMove = "unknown move";
+// Get computer move from function
+let computerMove = getMoveName(randomNumber);
 
-if (randomNumber == 1) {
-  computerMove = "rock";
-} else if (randomNumber == 2) {
-  computerMove = "paper";
-} else if (randomNumber == 3) {
-  computerMove = "scissors";
-}
-
-// Display computer move
-printMessage("Computer played: " + computerMove);
-
-// Player move
+// Get player move
 let playerInput = prompt("Choose your move: 1-rock, 2-paper, 3-scissors");
 
-let playerMove = "unknown move";
+// Convert player input to move name
+let playerMove = getMoveName(playerInput);
 
-if (playerInput == 1) {
-  playerMove = "rock";
-} else if (playerInput == 2) {
-  playerMove = "paper";
-} else if (playerInput == 3) {
-  playerMove = "scissors";
-}
-
-// Display player move
-printMessage("You played: " + playerMove);
-
-// Game result
-if (computerMove == playerMove) {
-  printMessage("Draw!");
-} else if (computerMove == "rock" && playerMove == "paper") {
-  printMessage("You win!");
-} else if (computerMove == "paper" && playerMove == "scissors") {
-  printMessage("You win!");
-} else if (computerMove == "scissors" && playerMove == "rock") {
-  printMessage("You win!");
-} else if (playerMove == "unknown move") {
-  printMessage("Wrong input!");
-} else {
-  printMessage("Computer wins!");
-}
+// Display result
+displayResult(computerMove, playerMove);
