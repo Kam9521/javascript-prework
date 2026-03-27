4704332e0d22927149654a11f3d91a2acffbead6
Kamil <szpmytyl@wp.pl>
2026-03-27 12:12:13 +0100

    Apply review fixes and improve HTML validation

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ let playerScore = 0;
 let computerScore = 0;
 // Function returns move name based on number
 function getMoveName(argMoveId) {
-  if (argMoveId == 1) {
+  if (argMoveId === 1) {
     return "rock";
-  } else if (argMoveId == 2) {
+  } else if (argMoveId === 2) {
     return "paper";
-  } else if (argMoveId == 3) {
+  } else if (argMoveId === 3) {
     return "scissors";
   } else {
     printMessage("Unknown move id: " + argMoveId);
@@ -15,24 +15,34 @@ function getMoveName(argMoveId) {
   }
 }
 
-// Function displays game result
+// Function displays game result and updates score
 function displayResult(argComputerMove, argPlayerMove) {
   printMessage("Computer played: " + argComputerMove);
   printMessage("You played: " + argPlayerMove);
 
-  if (argComputerMove == argPlayerMove) {
-    printMessage("Draw!");
-  } else if (argComputerMove == "rock" && argPlayerMove == "paper") {
-    printMessage("You win!");
-    playerScore++;
-  } else if (argComputerMove == "paper" && argPlayerMove == "scissors") {
-    printMessage("You win!");
-    playerScore++;
-  } else if (argComputerMove == "scissors" && argPlayerMove == "rock") {
-    printMessage("You win!");
-    playerScore++;
-  } else if (argPlayerMove == "unknown move") {
+  if (argPlayerMove === "unknown move") {
     printMessage("Wrong input!");
+    return;
+  }
+
+  if (argComputerMove === argPlayerMove) {
+    printMessage("Draw!");
+    return;
+  }
+
+  let playerWon = false;
+
+  if (argComputerMove === "rock" && argPlayerMove === "paper") {
+    playerWon = true;
+  } else if (argComputerMove === "paper" && argPlayerMove === "scissors") {
+    playerWon = true;
+  } else if (argComputerMove === "scissors" && argPlayerMove === "rock") {
+    playerWon = true;
+  }
+
+  if (playerWon === true) {
+    printMessage("You win!");
+    playerScore++;
   } else {
     printMessage("Computer wins!");
     computerScore++;
